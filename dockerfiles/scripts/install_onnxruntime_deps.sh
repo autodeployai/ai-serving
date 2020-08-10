73dbcd8e9b84144b312af259368cf220ee60fcef
scorebot <scorebot@outlook.com>
2020-08-10 13:31:51 +0000

    fix build error

--- dockerfiles/scripts/install_onnxruntime_deps.sh
+++ dockerfiles/scripts/install_onnxruntime_deps.sh
@@ -10,7 +10,7 @@ apt-get update && apt-get install -y --no-install-recommends \
         curl \
         libcurl4-openssl-dev \
         libssl-dev \
-        python3-dev \&&
+        python3-dev &&\
         rm -rf /var/lib/apt/lists/*
 
 # Dependencies: gradle
